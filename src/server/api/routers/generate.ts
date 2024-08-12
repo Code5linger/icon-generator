e8058345bc7fe06fe94401d19bd923afe0e2b230
Code5linger <sakib.ahmed.1708@gmail.com>
2024-08-12 13:36:28 +0600

    11. Setting up Credits

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,3 +1,5 @@
+import { TRPCClientError } from "@trpc/client";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
@@ -10,15 +12,28 @@ export const generateRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      const results = await ctx.prisma.user.updateMany({
+      const { count } = await ctx.prisma.user.updateMany({
         where: {
           id: ctx.session.user.id,
-          credite
+          credits: {
+            gte: 1,
+          },
         },
-        data: {},
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
       });
 
-      console.log(results);
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You do not have enough credits (T_T)",
+        });
+      }
+
+      // console.log(results);
       return {
         message: "Success",
       };
