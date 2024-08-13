8a3523cb547aa905faa50d0f94f7225cff09253a
Code5linger <sakib.ahmed.1708@gmail.com>
2024-08-13 07:11:56 +0600

    !

--- src/env.mjs
+++ src/env.mjs
@@ -1,9 +1,5 @@
 import { z } from "zod";
 
-/**
- * Specify your server-side environment variables schema here. This way you can ensure the app isn't
- * built with invalid env vars.
- */
 const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
@@ -21,22 +17,13 @@ const server = z.object({
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  DALLE_API_KEY: z.string(),
 });
 
-/**
- * Specify your client-side environment variables schema here. This way you can ensure the app isn't
- * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
- */
 const client = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
 });
 
-/**
- * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
- * middlewares) or client-side so we need to destruct manually.
- *
- * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
- */
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
@@ -44,6 +31,7 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  DALLE_API_KEY: process.env.DALLE_API_KEY,
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,8 +1,22 @@
 import { TRPCClientError } from "@trpc/client";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import OpenAI from "openai";
 
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { env } from "process";
+// API --
+const openai = new OpenAI({
+  // apiKey: process.env.OPENAI_API_KEY,
+  apiKey: env
+});
+
+const chatCompletion = await openai.chat.completions.create({
+  messages: [{ role: "user", content: "Say this is a test" }],
+  model: "gpt-4o-mini",
+});
+
+// API --
 
 export const generateRouter = createTRPCRouter({
   generateIcon: protectedProcedure
@@ -33,7 +47,6 @@ export const generateRouter = createTRPCRouter({
         });
       }
 
-      // console.log(results);
       return {
         message: "Success",
       };
